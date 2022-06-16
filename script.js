034d4323e1f379b94246ec0b896c0f6780c22fc8
Kim-Linh Vu <kimlinh@fb.com>
2022-06-16 16:22:01 -0700

    updated pop-up info

--- script.js
+++ script.js
@@ -8,59 +8,72 @@ const exit_button = document.querySelector(".close")
 const back_top_button = document.getElementById('back-to-top-btn')
 const overlay = document.getElementById('overlay')
 const container = document.querySelector('.container')
+const no_found = document.querySelector('.no-found')
 
 var page_num = 1;
-var card_id = 0;
-var movie_id = 0;
 var external_id = null;
-var key;
 
 function addCardEventListener() {
     let open = document.querySelectorAll('.movie-card')
     open.forEach(card => {
         card.addEventListener('click', () => {
-            // const modal = document.querySelector(card.dataset.modalTarget)
+            
             /* get movieid from modal attribute */
-            console.log(card.dataset.id)
             let movieId = card.dataset.id
             let overview = card.dataset.overview
 
             fetchVideo(movieId, overview)
-
             overlay.classList.add('show')
         })
     })
 }
 
-function addMovieHTML(movieGridElement, movie){
-    /* adjust length of movie title */
-    let newTitle = movie.title
-    if (movie.title.length > 17){
-        newTitle = movie.title.slice(0, 17)
-        newTitle = newTitle.concat("...")
+function addMovie(movieGridElement, movies){
+    movies.forEach(movie => {
+        /* adjust length of movie title */
+        let newTitle = movie.title
+        if (movie.title.length > 17){
+            newTitle = movie.title.slice(0, 17)
+            newTitle = newTitle.concat("...")
+        }
+        
+        /* return html in another function */
+        movieGridElement.innerHTML += getMovieHTML(movie, newTitle)
+        
+    })  
+
+    if (movie_grid.scrollHeight > 1000){
+        back_top_button.classList.remove('hidden')
     }
-    /* add data-movie id to movie-card div */
-    /* return html in another function */
-    movieGridElement.innerHTML += `
+}
+
+function getMovieHTML(movie, newTitle){
+    return `
     <div class="movie-card" data-id="${movie.id}" data-overview="${movie.overview}">
         <img class="movie-poster" src="${imageBaseUrl}/w342${movie.poster_path}" alt="${movie.title}" title="${movie.title}"/>
         <h2 class="movie-title">${newTitle}</h2>
         <h3 class="movie-votes">Ratings: ${movie.vote_average}</h3>
-    </div>
+     </div>
+        
     `
-    // fetchVideo(api_key, movie, modal_container)   
-
-    
-    if (movie_grid.scrollHeight > 1000){
-        back_top_button.classList.remove('hidden')
-        console.log("here")
-    }
 }
 
-function addMovies(movies){
-    movies.forEach(movie => {
-        addMovieHTML(movie_grid, movie)
-    })
+function getModalHTML(overview, key, flag){
+    if (flag == 1){
+        return `
+        <div class="modal-card show">
+            <p>${overview}</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}"></iframe>
+        </div>    
+        `
+    }
+    else{
+        return `
+        <div class="modal-card show">
+            <p>${overview}</p>
+        </div>
+        `
+    }
 }
 
 function addEventListeners(loadMoreButton, searchInput, exitButton, backToTopBtn, overlay){
@@ -106,25 +119,15 @@ const fetchVideo = async (movieId, overview) => {
     try {
         const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${api_key}&language=en-US`);
         const data = await res.json();
-        key = data.results[0].key
+        let key = data.results[0].key
         
         /* put in another function */
-        modal_container.innerHTML = `
-        <div class="modal-card show">
-            <p>${overview}</p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}"></iframe>
-        </div>
-        `
-        modal_container.classList.remove('hidden')
+        modal_container.innerHTML = getModalHTML(overview, key, 1)
+        
     } catch (err) {
-        console.log(err)
-        modalContainer.innerHTML += `
-        <div class="modal-card" id="modal-card${card_id++}">
-            <h2>More About ${movie.title}</h2>
-            <p>${movie.overview}</p>
-        </div>
-        `
+        modal_container.innerHTML = getModalHTML(overview, 0, 0)
     }
+    modal_container.classList.remove('hidden')
 }
 
 const fetchMovies = async (apiKey, external_id) => {
@@ -132,30 +135,37 @@ const fetchMovies = async (apiKey, external_id) => {
         try{
             const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${external_id}&page=${page_num}&include_adult=false`);
             const data = await res.json();
-            console.log(data.results)
+
             load_more_button.classList.remove('hidden')
 
             if (data.results.length == 0){
-                movie_grid.innerHTML = `
-                <h1>No Movies Found</h1>
+                movie_grid.innerHTML = ``
+                no_found.innerHTML = `
+                <h1>No Movie Found</h1>
                 `
+                hideButtons()
+            }else{
+                addMovie(movie_grid, data.results)
             }
-            addMovies(data.results)
         } catch (err){
-            console.log(err)
+
         }
     }
     else{
         try {
             const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page_num}`);
             const data = await res.json();
-            addMovies(data.results)
+            addMovie(movie_grid, data.results)
         } catch (err){
             console.error(err)
         }
     }
-    
     addCardEventListener();
+}
+
+function hideButtons(){
+    back_top_button.classList.add('hidden')
+    load_more_button.classList.add('hidden')
 }
 
 function resetVars(){
@@ -164,6 +174,8 @@ function resetVars(){
     page_num = 1
     movie_id = 0
     card_id = 0
+    no_found.innerHTML = ``
+    load_more_button.classList.remove('hidden')
 }
 
 window.onload = function () {
